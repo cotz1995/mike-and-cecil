a7700c8542ce5d02c88362646a7d3a3c5f2b3835
Cory Lotz <cotz1995@gmail.com>
2021-11-25 09:39:27 -0500

    changes to improve card gallery UI  * display of new lines in messages  * calc card rowSpan off rendered msg height rather than char count

--- ui/src/app/card-gallery/card-gallery.component.ts
+++ ui/src/app/card-gallery/card-gallery.component.ts
@@ -11,19 +11,21 @@ export class CardGalleryComponent {
   @Input()
   messages: Message[] = []
 
+  rowSpans = new Map<number, number>()
+
   pxToEmRatio = 16
   columns = 1
+  cardWidth = 15
   rowHeight = 13
+  gutterSize = 1
+
   public get rowHeightStr () {
     return `${this.rowHeight}em`
   }
 
-  gutterSize = 1
   public get gutterSizeStr () {
     return `${this.gutterSize}em`
   }
-
-  cardWidth = 15
 
   calculateColumns (width: number): number {
     return Math.trunc(width / (15 * this.pxToEmRatio))
@@ -36,20 +38,29 @@ export class CardGalleryComponent {
     return columnWidth - this.cardWidth + 1
   }
 
-  onResized (event: ResizedEvent) {
+  onBoardResized (event: ResizedEvent) {
     const width = event.newRect.width
     this.columns = this.calculateColumns(width)
     this.gutterSize = this.calculateGutter(width, this.columns)
   }
 
-  calculateRowSpan (message: Message): number {
-    const rowspan = message.message.length / 265
+  onMessageResized (event: ResizedEvent, index: number) {
+    this.rowSpans.set(index, this.calculateRowSpan(event.newRect.height))
+  }
 
-    return Math.trunc(rowspan + 1)
+  calculateRowSpan (heightPx: number): number {
+    const heightEm = heightPx / this.pxToEmRatio
+    const rowspan = heightEm / this.rowHeight + 1
+    return Math.trunc(rowspan)
+  }
+
+  getRowSpan (index: number): number {
+    return this.rowSpans.get(index) ?? 1
   }
 
   cardStyle (rowspan: number): string {
-    const height = 10 + (rowspan - 1) * (this.rowHeight + this.gutterSize)
-    return `height: ${height}em`
+    const heightOfSingleCard = 10
+    const totalCardHeight = heightOfSingleCard + (rowspan - 1) * (this.rowHeight + this.gutterSize)
+    return `height: ${totalCardHeight}em`
   }
 }
