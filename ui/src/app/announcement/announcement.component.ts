933b05f95e1e6367115f3920af558d097e3a89fe
Garrick Suemith <garrick.suemith@gmail.com>
2021-12-15 22:43:07 -0500

    added additional links and pointers

--- ui/src/app/announcement/announcement.component.ts
+++ ui/src/app/announcement/announcement.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core'
   templateUrl: './announcement.component.html',
   styleUrls: ['./announcement.component.css']
 })
-export class AnnouncementComponent {}
+export class AnnouncementComponent {
+
+  obitUrl = 'https://www.millerfuneralhome.net/obituaries/Cecilia-Suemith/#!/Obituary'
+  flowersUrl = 'https://www.millerfuneralhome.net/store/Category.aspx?CategoryId=319&icn=desktop_memorial_topbanner'
+  goFundMeUrl = 'https://www.gofundme.com/f/mike-and-cecil'
+  dadFacebookUrl = 'https://www.facebook.com/msuemith'
+}
